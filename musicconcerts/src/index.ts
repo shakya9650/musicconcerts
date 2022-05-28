f99364ff4438a9e61ea19d4d995d6d7346ae3e27
Shivam shakya <shivamshakys6265@gmail.com>
2022-05-28 20:31:59 +0530

    music concerts

---
+++ musicconcerts/src/index.ts
@@ -0,0 +1,139 @@
+import { KeyDisplay } from './utils';
+import { CharacterControls } from './characterControls';
+import * as THREE from 'three'
+import { CameraHelper } from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+//
+
+
+// SCENE
+const scene = new THREE.Scene();
+scene.background = new THREE.Color(0xa8def0);
+
+// CAMERA
+const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.y = 5;
+camera.position.z = 5;
+camera.position.x = 0;
+
+
+// RENDERER
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.shadowMap.enabled = true
+
+// CONTROLS
+const orbitControls = new OrbitControls(camera, renderer.domElement);
+orbitControls.enableDamping = true
+orbitControls.minDistance = 5
+orbitControls.maxDistance = 15
+orbitControls.enablePan = false
+orbitControls.maxPolarAngle = Math.PI / 2 - 0.05
+orbitControls.update();
+
+// LIGHTS
+light()
+
+// FLOOR
+generateFloor()
+
+// MODEL WITH ANIMATIONS
+var model;
+var characterControls: CharacterControls
+new GLTFLoader().load('models/Soldier.glb', function (gltf) {
+  model = gltf.scene;
+ 
+    model.position.set(1,0,30)
+    scene.add(model);
+
+
+ 
+
+    
+    const gltfAnimations: THREE.AnimationClip[] = gltf.animations;
+    const mixer = new THREE.AnimationMixer(model);
+    const animationsMap: Map<string, THREE.AnimationAction> = new Map()
+    gltfAnimations.filter(a => a.name != 'TPose').forEach((a: THREE.AnimationClip) => {
+        animationsMap.set(a.name, mixer.clipAction(a))
+    })
+
+    characterControls = new CharacterControls(model, mixer, animationsMap, orbitControls, camera,  'Idle')
+});
+
+// CONTROL KEYS
+const keysPressed = {  }
+const keyDisplayQueue = new KeyDisplay();
+document.addEventListener('keydown', (event) => {
+    keyDisplayQueue.down(event.key)
+    if (event.shiftKey && characterControls) {
+        characterControls.switchRunToggle()
+    } else {
+        (keysPressed as any)[event.key.toLowerCase()] = true
+    }
+}, false);
+document.addEventListener('keyup', (event) => {
+    keyDisplayQueue.up(event.key);
+    (keysPressed as any)[event.key.toLowerCase()] = false
+}, false);
+
+const clock = new THREE.Clock();
+
+
+
+// ANIMATE
+function animate() {
+    let mixerUpdateDelta = clock.getDelta();
+    if (characterControls) {
+        characterControls.update(mixerUpdateDelta, keysPressed);
+    }
+    
+    orbitControls.update()
+    renderer.render(scene, camera);
+    requestAnimationFrame(animate);
+}
+document.body.appendChild(renderer.domElement);
+animate();
+
+// RESIZE HANDLER
+function onWindowResize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    keyDisplayQueue.updatePosition()
+}
+window.addEventListener('resize', onWindowResize);
+
+function generateFloor() {
+    const gltfLoader = new GLTFLoader();
+    gltfLoader.load("/models/concert v2.glb", (gltf) => {
+        gltf.scene.scale.set(1, 1, 1);
+        scene.add(gltf.scene);
+      
+       
+        
+      });
+      
+   
+}
+
+
+
+function light() {
+    scene.add(new THREE.AmbientLight(0xffffff, 0.7))
+
+    const dirLight = new THREE.DirectionalLight(0xffffff, 1)
+    dirLight.position.set(- 60, 100, - 10);
+    dirLight.castShadow = true;
+    dirLight.shadow.camera.top = 50;
+    dirLight.shadow.camera.bottom = - 50;
+    dirLight.shadow.camera.left = - 50;
+    dirLight.shadow.camera.right = 50;
+    dirLight.shadow.camera.near = 0.1;
+    dirLight.shadow.camera.far = 200;
+    dirLight.shadow.mapSize.width = 4096;
+    dirLight.shadow.mapSize.height = 4096;
+    scene.add(dirLight);
+    // scene.add( new THREE.CameraHelper(dirLight.shadow.camera))
+}
